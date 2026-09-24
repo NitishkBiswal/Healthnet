2b700b8f31dbae9df9faf2035f79f700f42f4beb
NitishkBiswal <biswalnitish08@gmail.com>
2026-09-24 13:41:09 +0530

    fix(segment-0): type FHIR mapper stubs

--- backend/app/fhir/resource_mapper.py
+++ backend/app/fhir/resource_mapper.py
@@ -1,12 +1,13 @@
-from typing import Any, Dict
+from typing import Any
+
 
 class FHIRResourceMapper:
     """Maps between HealthNet internal models and FHIR R4 resources."""
 
-    def to_fhir(self, internal_model: Any) -> Dict[str, Any]:
+    def to_fhir(self, internal_model: Any) -> dict[str, Any]:
         # TODO: Implement in Segment 7
-        pass
+        raise NotImplementedError("FHIR resource mapping is implemented in Segment 7")
 
-    def from_fhir(self, fhir_resource: Dict[str, Any]) -> Any:
+    def from_fhir(self, fhir_resource: dict[str, Any]) -> Any:
         # TODO: Implement in Segment 7
-        pass
+        raise NotImplementedError("FHIR resource mapping is implemented in Segment 7")
